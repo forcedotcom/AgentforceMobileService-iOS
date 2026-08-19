1a70cba802aff7bd72781208bd712532387bfbfa
Alex Sikora <asikora@salesforce.com>
2026-08-19 15:52:58 -0700

    Add SPM binary distribution manifest for 6.11.5-rc1 (262.2-RC1 RC)

--- Package.swift
+++ Package.swift
@@ -40,8 +40,8 @@ let package = Package(
             // For a hosted release, replace with:
             //   url: "https://<host>/AgentforceService-<version>.xcframework.zip",
             //   checksum: "<output of swift package compute-checksum>"
-            url: "https://github.com/forcedotcom/AgentforceMobileService-iOS/releases/download/6.11.4/AgentforceMobileService-262-1-3-spm.xcframework.zip",
-            checksum: "26e0520a615207dcafc80c25d76bf27df7ab0720c922a83090a027a8d96395a0"
+            url: "https://github.com/forcedotcom/AgentforceMobileService-iOS/releases/download/6.11.5-rc1/AgentforceMobileService-262-2-RC1-spm.xcframework.zip",
+            checksum: "f7e6ab616c7b3aa4bffeb6c0b9c5c6954666166d559b86c1c10c79901f2beedd"
         ),
         .target(
             name: "AgentforceServiceTarget",
